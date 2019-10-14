be965e273c947f9cd36a623dfc24cf95ba5f3298
Serg Kizim <sergkizim@gmail.com>
2019-10-14 18:30:28 +0300

    Add User.balance field

--- src/schema/User.ts
+++ src/schema/User.ts
@@ -1,4 +1,4 @@
-import { objectType } from 'nexus';
+import { objectType, arg } from 'nexus';
 
 export const User = objectType({
   name: 'User',
@@ -6,5 +6,17 @@ export const User = objectType({
     t.model.id(),
     t.model.name()
     t.model.accounts()
+    t.field('balance', {
+      type: 'Float',
+      args: {
+        currency: arg({ type: 'Currency', required: true }),
+      },
+      resolve: async ({ id }, { currency }, { photon, datasources }) => {
+        const accounts = await photon.accounts.findMany({ where: { user: { id } }, select: { balance: true, currency: true } });
+        const balances = await Promise.all(accounts.map(a => datasources.exchangeRatesAPI.convert(a.balance, a.currency, currency)));
+
+        return balances.reduce((p, v) => p + v, 0);
+      },
+    })
   },
 });
