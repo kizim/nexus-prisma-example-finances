f6005cb1569eb968744323b217dd19914d8180ec
Serg Kizim <sergkizim@gmail.com>
2019-10-14 17:39:50 +0300

    Update resolver for Account.balance field

--- src/schema/Account.ts
+++ src/schema/Account.ts
@@ -8,23 +8,16 @@ export const Account = objectType({
     t.model.currency(),
     t.model.user()
     t.model.transactions({ filtering: true }),
-    t.model.balance()
-    // t.field('balance', {
-    //   type: 'Float',
-    //   args: {
-    //     currency: arg({ type: 'Currency' }),
-    //   },
-    //   resolve: async ({ id, currency: baseCurrency }, { currency }, { photon, datasources }) => {
-    //     const transactions = await photon.transactions.findMany({ where: { account: { id } }, select: { amount: true } });
-
-    //     const balance = transactions.reduce((r, t) => r + t.amount, 0);
-
-    //     const total = currency
-    //       ? await datasources.exchangeRatesAPI.convert(balance, baseCurrency, currency)
-    //       : balance;
-
-    //     return Math.round(total * 100) / 100;
-    //   }
-    // })
+    t.field('balance', {
+      type: 'Float',
+      args: {
+        currency: arg({ type: 'Currency' }),
+      },
+      resolve: async ({ balance, currency: baseCurrency }, { currency }, { datasources }) => {
+        return (currency && currency !== baseCurrency)
+          ? await datasources.exchangeRatesAPI.convert(balance, baseCurrency, currency)
+          : balance;
+      },
+    })
   },
 });
