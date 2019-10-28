2f5b852c03cc0ebcc8e96c4bbed94e82e3e96ebe
Serg Kizim <sergkizim@gmail.com>
2019-10-28 12:02:07 +0300

    Remove try because Prisma findOne doesn't throw error anymore

--- src/helpers/get-user.ts
+++ src/helpers/get-user.ts
@@ -4,21 +4,10 @@ import { User } from '@generated/photon';
 import photon from '../photon';
 
 export default async ({ authorization }: { authorization: string }): Promise<User | null> => {
-  try {
-    const token: any = jwt.verify(authorization.replace('Bearer ', ''), 'secret');
-
-    // const user = {
-    //   id: token.sub,
-    //   email: token.email,
-    //   name: token.name,
-    //   roles: token.roles,
-    // };
-    
-    const user = await photon.users.findOne({ where: { id: token.sub }});
-    delete user.password;
-
-    return user;
-  } catch (e) {
-    return null;
-  }
+  const token: any = jwt.verify(authorization.replace('Bearer ', ''), 'secret');
+  
+  const user = await photon.users.findOne({ where: { id: token.sub }});
+  if (user) delete user.password;
+  
+  return user;
 };
--- src/schema/Mutation.ts
+++ src/schema/Mutation.ts
@@ -1,5 +1,5 @@
 import { mutationType, stringArg } from 'nexus';
-import { UserInputError } from 'apollo-server';
+import { UserInputError, ForbiddenError } from 'apollo-server';
 
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
@@ -14,29 +14,24 @@ export const Mutation = mutationType({
         password: stringArg()
       },
       resolve: async (_, { email, password }, { photon }) => {
-        try {
-          await photon.users.findOne({ where: { email } });
-        } catch (e) {
-          const hashedPassword = await bcrypt.hash(password, 10);
+        const existingUser = await photon.users.findOne({ where: { email } });
+        if (existingUser) throw new UserInputError('This email is already exist in our system');
 
-          console.log('test');
+        const hashedPassword = await bcrypt.hash(password, 10);
 
-          const user = await photon.users.create({ data: { email, password: hashedPassword } });
+        const user = await photon.users.create({ data: { email, password: hashedPassword } });
 
-          const idToken = jwt.sign({
-            email: user.email,
-            name: user.name,
-            roles: user.roles,
-            sub: user.id,
-            iss: 'https://auth.fujix.io/basic'
-          }, 'secret', {
-            algorithm: 'HS256',
-          });
+        const idToken = jwt.sign({
+          email: user.email,
+          name: user.name,
+          roles: user.roles,
+          sub: user.id,
+          iss: 'https://auth.fujix.io/basic'
+        }, 'secret', {
+          algorithm: 'HS256',
+        });
 
-          return { idToken, user };
-        }
-
-        throw new UserInputError('This email is already exist in our system');
+        return { idToken, user };
       }
     }),
     t.field('signIn', {
@@ -46,16 +41,11 @@ export const Mutation = mutationType({
         password: stringArg()
       },
       resolve: async (_, { email, password }, { photon }) => {
-        let user;
-        try {
-          user = await photon.users.findOne({ where: { email } });
-          if (!user.password) throw new Error();
+        const user = await photon.users.findOne({ where: { email } });
+        if (!user || !user.password) throw new ForbiddenError('Incorrect email or password');
 
-          const passwordMatch = await bcrypt.compare(password, user.password as string);
-          if (!passwordMatch) throw new Error();
-        } catch (e) {
-          throw new UserInputError('Invalid email or password');
-        }
+        const passwordMatch = await bcrypt.compare(password, user.password as string);
+        if (!passwordMatch) throw new ForbiddenError('Incorrect email or password');
 
         const idToken = jwt.sign({
           email: user.email,
