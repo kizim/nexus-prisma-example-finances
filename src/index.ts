ad8c26356829c7dcfa1d861a975bdb835bc2fdb8
Eugene Barannik <wellfail@milkandcartoons.com>
2019-10-29 17:06:36 +0300

    Add prisma-engine queries route

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,39 @@
-import { ApolloServer } from 'apollo-server';
-import { applyMiddleware } from 'graphql-middleware'
+import { ApolloServer } from 'apollo-server-express';
+import { applyMiddleware } from 'graphql-middleware';
+import express from 'express';
+import bodyParser from 'body-parser';
+import { Request, Response } from 'express';
+
 
 import schema from './schema';
+import photon from './photon';
 import { createContext } from './context';
 import * as middlewares from './middlewares';
+import request from 'request-promise';
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/asketes', async (req: Request, res: Response) => {
+  if (!process.env.ENGINE_PORT) {
+    await photon.connect();
+    //@ts-ignore
+    process.env.ENGINE_PORT = photon.fetcher.engine.port;
+    console.log(`Prisma engine ready at http://localhost:${process.env.ENGINE_PORT}`)
+  }
+
+  const uri = `http://localhost:${process.env.ENGINE_PORT}/`;
+  //@ts-ignore
+  let result;
+  try {
+    result = await request.post(uri, {
+      headers: req.headers,
+      body: JSON.stringify(req.body),
+    });
+  } catch(e) {}
+
+  res.status(200).end(result);
+});
 
 const server = new ApolloServer({
   schema: applyMiddleware(schema, middlewares.permissions, middlewares.resolvers),
@@ -11,7 +41,8 @@ const server = new ApolloServer({
   tracing: true,
   introspection: true,
 });
+server.applyMiddleware({ app, path: '/'});
 
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+app.listen({ port: 4000 }, () =>
+  console.log(`🚀  Server ready at http://localhost:4000${server.graphqlPath}`),
+);
